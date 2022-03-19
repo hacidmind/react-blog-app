6f645caaf97e337dd18f0160ede957986db9ed26
hacidmind <abiolahafeez@yahoo.com>
2022-03-19 14:32:13 +0100

    add .env file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // development URL
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_DEVELOPMENT}/api/v1`;
+}
+// else {
+//   // production URL
+//   axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_PRODUCTION}/api/v1`;
+// }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
